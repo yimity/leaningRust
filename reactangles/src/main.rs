abde1180e294518744c65e6df8a38641bb187a56
yimity <yimity@gmail.com>
2022-10-25 10:31:13 +0800

    结构体

--- reactangles/src/main.rs
+++ reactangles/src/main.rs
@@ -1,36 +1,30 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn height(&self) -> bool {
+        self.height > 0
+    }
+    fn square(size: u32) -> Rectangle {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let width = 30;
-    let height = 50;
-
-    let rect = (30, 50);
-
-    let reactangle = Reactangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    // let result = area1(width, height);
-
-    // let result = area2(rect);
-
-    let result = area3(&reactangle);
-
-    println!("The area of the rectangle is {} square pixels.", result);
-}
-
-struct Reactangle {
-    width: u32,
-    height: u32,
-}
-
-fn area1(width: u32, height: u32) -> u32 {
-    width * height
-}
-
-fn area2(wh: (u32, u32)) -> u32 {
-    return wh.0 * wh.1;
-}
-
-fn area3(reactangle: &Reactangle) -> u32 {
-    return reactangle.width * reactangle.height;
+    if rect1.width() || rect1.height() {
+        println!("The rectangle has a nonzero width; it is {:?}", Rectangle::square(4));
+    }
 }
